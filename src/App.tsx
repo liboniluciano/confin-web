d4ac176e7dce735a9c71e717e05a66642dfad6f7
Luciano Liboni <luciano_liboni@hotmail.com>
2020-09-29 19:28:49 -0300

    :heavy_plus_sign: add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+
 import { AuthProvider } from './contexts/auth';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import './assets/styles/global.css';
 import Routes from './routes';
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <>
+      <ToastContainer autoClose={3000} />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </>
   );
 }
 
