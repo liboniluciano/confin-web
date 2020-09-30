186db03660972b3e7e8b4cee13da8026c5c7ad0f
Luciano Liboni <luciano_liboni@hotmail.com>
2020-09-30 01:23:01 -0300

    :sparkles: change header options if signed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts/auth';
+
 
 import { MenuContent, ItemMenu, HeaderImg } from './styles';
 
 
 const Header: React.FC = () => {
+  const { signed } = useAuth();
+
   return (
     <MenuContent>
-      <Link to="/teste" style={{ textDecoration: 'none' }}>
-      <ItemMenu>TRANSAÇÕES</ItemMenu>
-      </Link>
+      {signed ? <> 
+        <Link to="/teste" style={{ textDecoration: 'none' }}>
+        <ItemMenu>TRANSAÇÕES</ItemMenu>
+        </Link>
+      </> : null }
       <Link to="/" style={{ textDecoration: 'none' }}>
         <ItemMenu>
           <HeaderImg />
         </ItemMenu>
       </Link>
+      {signed ? <> 
       <Link to="/about" style={{ textDecoration: 'none' }}>
         <ItemMenu>SAIR</ItemMenu>
       </Link>
+      </> : null }
     </MenuContent>
   );
 }
